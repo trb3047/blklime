9acc7170a95dfbab0230c687ccf26c5238849cd2
trb3047 <hoon_1215@naver.com>
2024-05-22 19:27:39 +0900

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}", "./src/***/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
